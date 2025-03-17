b3ad708f08d3da862885429623205e106d39da01
MadMadCoder <Madison.M28@school.mountdesales.org>
2025-03-17 15:20:50 -0400

    clean up room

--- src/FirstPackage/SoftballPlayer.java
+++ src/FirstPackage/SoftballPlayer.java
@@ -4,11 +4,22 @@ public class SoftballPlayer {
 	String fn;
 	String ln;
 	String jerseyNum;
+	Position position;
+	
+	/*
+	 * Modify the constructor
+	 * Modify the toString
+	 * Modify main to print the position abbreviation
+	 */
 
-	public SoftballPlayer(String fn, String ln, String jerseyNum) {
+	public SoftballPlayer(String fn, String ln, String jerseyNum, Position pos) {
 		this.fn = fn;
 		this.ln = ln;
 		this.jerseyNum = jerseyNum;
+		this.position = pos;
+		
+	
+	
 
 	}
 
@@ -20,42 +31,54 @@ public class SoftballPlayer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My First Program");
-			
-		SoftballPlayer catcher = new SoftballPlayer("Adley", "Rustchman", "35");
-		System.out.println(catcher);
-		
-		SoftballPlayer leftfielder= new SoftballPlayer ("Cedric",  "Mullins" , "31");
-		System.out.println(leftfielder);
-		
-		SoftballPlayer pitcher = new SoftballPlayer("Fred", "Fastball", "99");
-		System.out.println(pitcher);
-		
-		SoftballPlayer rightfielder= new SoftballPlayer ("Tlyer", "O'Neil", "24");
-		
-		SoftballPlayer centerfielder= new SoftballPlayer ("Colton", "Cowser", "17");
-		
-		SoftballPlayer firstbaseman= new SoftballPlayer ( "Ryan", "Mountcastle", "06");
-		
-		SoftballPlayer secondbaseman= new SoftballPlayer ("Jackson" , "Holiday", "07");
-		
-		SoftballPlayer thirdbaseman= new SoftballPlayer ("Ramon", "Urias", "29");
-		
-		SoftballPlayer shortstop= new SoftballPlayer ("Gunnar", "Herderson", "02");
-		
-		SoftballPlayer dhitter= new SoftballPlayer ("David" ,"Oritz", "34");
-		
 		SoftballPlayer[] roster = new SoftballPlayer [10];
-		roster [0]  = catcher;
-		roster [1]= pitcher;
-		roster [2]= leftfielder;
-		roster [3]= rightfielder;
-		roster [4]= centerfielder;
-		roster [5]=firstbaseman;
-		roster [6]= secondbaseman;
-		roster [7]= thirdbaseman;
-		roster [8]= shortstop;
-		roster [9]= dhitter;
-		System.out.println("array size is " + roster.length);
+		
+		
+		SoftballPlayer player = new SoftballPlayer("Adley", "Rustchman", "35", Position.CACTHER);
+		roster [0]  = player;
+		System.out.println(player);
+		
+		
+		player= new SoftballPlayer ("Cedric",  "Mullins" , "31" , Position.LEFT_FIELD);
+		roster [1]  = player;
+		System.out.println(player);
+		
+		
+		player = new SoftballPlayer("Fred", "Fastball", "99", Position.PITCHER );
+		roster [2]  = player;
+		System.out.println(player);
+		
+		 player= new SoftballPlayer ("Tlyer", "O'Neil", "24", Position.RIGHT_FIELD);
+		 roster [3]  = player;
+		 System.out.println(player);
+		
+		 player= new SoftballPlayer ("Colton", "Cowser", "17", Position.CENTER_FIELD);
+		 roster [4]  = player;
+		 System.out.println(player);
+		 
+		 player= new SoftballPlayer ( "Ryan", "Mountcastle", "06", Position.FIRST_BASEMAN);
+		 roster [5]  = player;
+		 System.out.println(player);
+		 
+		 player= new SoftballPlayer ("Jackson" , "Holiday", "07", Position.SECOND_BASEMAN);
+		 roster [6]  = player;
+		 System.out.println(player);
+		
+		 player= new SoftballPlayer ("Ramon", "Urias", "29", Position.THIRD_BASEMAN);
+		 roster [7]  = player;
+		 System.out.println(player);
+		
+		 player= new SoftballPlayer ("Gunnar", "Herderson", "02", Position.SHORTSTOP);
+		 roster [8]  = player;
+		 System.out.println(player);
+		
+		 player= new SoftballPlayer ("David" ,"Oritz", "34", Position.DHitter);
+		 roster [9]  = player;
+		 System.out.println(player);
+		
+	
+		
+	System.out.println("\narray size is " + roster.length + "\n");
 		
 		for (int i=0; i < roster.length; i++) {
 			  System.out.println(roster[i]);
